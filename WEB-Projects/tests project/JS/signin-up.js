4c1a42bc640879a29653def7487e28b5c4a17afa
izat harb <izatha101@gmail.com>
2021-09-08 18:36:48 +0300

    mainweb

--- WEB-Projects/tests project/JS/signin-up.js
+++
@@ -1,9 +0,0 @@
-function signup() {
-    "use strict";
-    document.getElementById("signupid").style = "left:0;opacity: 1;transition: 1s all; z-index:99";
-};
-
-function signin() {
-    "use strict";
-    document.getElementById("signupid").style = "left:-100;opacity: 0;transition: 1s all; z-index: -1";
-}
